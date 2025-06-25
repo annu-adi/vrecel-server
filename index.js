00fb00fe5db78de0cf27011de2b7a92af5c13566
annu-adi <rashmi.maurya10@gmail.com>
2025-06-26 00:39:20 +0530

    new

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const recipeRouter = require('./Routes/reciper')
 const app = express()
 
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["https://vercel-recipe-seven.vercel.app"],
     methods:["GET","POST","PUT"],
     credentials:true
 }))
